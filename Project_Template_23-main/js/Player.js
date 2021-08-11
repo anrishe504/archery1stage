c60cb115c0aecb2841d077de54465d13245862d0
anrishe504 <84959653+anrishe504@users.noreply.github.com>
2021-08-11 08:42:53 +0530

    Add files via upload

---
+++ Project_Template_23-main/js/Player.js
@@ -0,0 +1,28 @@
+class Player {
+    constructor(x,y,width,height){
+
+      var options= {
+        isStatic :true
+      }
+  
+  
+      this.body = Bodies.rectangle(x,y,width,height,options)
+      World.add(world, this.body)
+      this.w = width;
+      this.h = height;
+      this.image = loadImage("assets/player.png");
+            
+    }
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+     
+      push();
+      translate(pos.x,pos.y)
+      rotate(angle);
+      imageMode(CENTER)
+      image(this.image, 0,0, this.w, this.h);
+      pop();
+    }
+    
+  }
